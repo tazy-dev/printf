2276418928b96397db6da3f8a5ab5581dfbddf69
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-09-22 07:15:07 -0700

    Task1

---
+++ print_functions_radix.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_int - Print integer to console
+ *
+ * @ap : input argument
+ * Return: The number of digits of the output
+ */
+int print_int(va_list ap)
+{
+	int num = va_arg(ap, int);
+
+	return (_putString(convert_number(num, 10, 0)));
+}
