3659fdc4c53b093aa3cf3e0c73ed1d88d4eb3377
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-09-22 10:41:51 -0700

    Task2

--- print_functions_radix.c
+++ print_functions_radix.c
@@ -12,3 +12,16 @@ int print_int(va_list ap)
 
 	return (_putString(convert_number(num, 10, 0)));
 }
+
+/**
+ * print_bin - Print integer as Binary to console
+ *
+ * @ap : input argument
+ * Return: The number of digits of the output
+ */
+int print_bin(va_list ap)
+{
+	unsigned int num = va_arg(ap, unsigned int);
+
+	return (_putString(convert_number(num, 2, UNSIGNED)));
+}
