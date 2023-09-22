9f88d3ca36a11ad5fcaf559ca3f120902166ac5b
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-09-22 07:14:18 -0700

    Task1

--- util.c
+++ util.c
@@ -14,6 +14,8 @@ int (*function_pointer(char s))(va_list)
 		{'c', print_char},
 		{'%', print_percentage},
 		{'s', print_string},
+		{'d', print_int},
+		{'i', print_int},
 		{' ', NULL}};
 
 	while (specifier_list[index].specifier != ' ')
@@ -52,3 +54,29 @@ int sprcifier_function(char s, va_list ap)
  * Return: pointer to the resulting string
  *
 */
+char *convert_number(int num, int base, int check)
+{
+	static char *conv_table;
+	static char res_buf[64];
+	unsigned int n = num;
+	char sign = 0;
+	char *ptr;
+
+	if (!(check & UNSIGNED) && (num < 0))
+	{
+		n = -num;
+		sign = '-';
+	}
+	conv_table = (check & LOWERCASE) ? "0123456789abcdef" : "0123456789ABCDEF";
+	ptr = &res_buf[63];
+	*ptr = '\0';
+	do {
+		*--ptr = conv_table[n % base];
+		n /= base;
+	} while (n != 0);
+	if (sign)
+	{
+		*--ptr = sign;
+	}
+	return (ptr);
+}
